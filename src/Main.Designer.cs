c247eb2c48397ce3c4ece857c8d831f6d7aafe34
exsersewo <exsersewo@systemexit.co.uk>
2020-11-09 23:49:26 +0000

    Init

---
+++ src/Main.Designer.cs
@@ -0,0 +1,407 @@
+﻿
+namespace LastRPC
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.notifier = new System.Windows.Forms.NotifyIcon(this.components);
+			this.notifyMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.logTextBox = new System.Windows.Forms.RichTextBox();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.tableLayoutPanel5 = new System.Windows.Forms.TableLayoutPanel();
+			this.label7 = new System.Windows.Forms.Label();
+			this.lastFMUsername = new System.Windows.Forms.TextBox();
+			this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+			this.label6 = new System.Windows.Forms.Label();
+			this.discordAppId = new System.Windows.Forms.TextBox();
+			this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+			this.label5 = new System.Windows.Forms.Label();
+			this.lastFMSecret = new System.Windows.Forms.TextBox();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.label4 = new System.Windows.Forms.Label();
+			this.lastFMKey = new System.Windows.Forms.TextBox();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.start = new System.Windows.Forms.Button();
+			this.stop = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.statusStrip1.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.tableLayoutPanel5.SuspendLayout();
+			this.tableLayoutPanel4.SuspendLayout();
+			this.tableLayoutPanel3.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 424);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(800, 26);
+			this.statusStrip1.TabIndex = 2;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// statusLabel
+			// 
+			this.statusLabel.Name = "statusLabel";
+			this.statusLabel.Size = new System.Drawing.Size(50, 20);
+			this.statusLabel.Text = "Ready";
+			// 
+			// notifier
+			// 
+			this.notifier.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+			this.notifier.BalloonTipText = "Sample";
+			this.notifier.BalloonTipTitle = "LastRPC";
+			this.notifier.ContextMenuStrip = this.notifyMenuStrip;
+			this.notifier.Icon = ((System.Drawing.Icon)(resources.GetObject("notifier.Icon")));
+			this.notifier.Text = "LastRPC";
+			this.notifier.Visible = true;
+			this.notifier.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifier_MouseDoubleClick);
+			// 
+			// notifyMenuStrip
+			// 
+			this.notifyMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.notifyMenuStrip.Name = "notifyMenuStrip";
+			this.notifyMenuStrip.Size = new System.Drawing.Size(61, 4);
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.logTextBox);
+			this.panel1.Controls.Add(this.panel2);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(800, 424);
+			this.panel1.TabIndex = 3;
+			// 
+			// logTextBox
+			// 
+			this.logTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.logTextBox.Location = new System.Drawing.Point(0, 0);
+			this.logTextBox.Name = "logTextBox";
+			this.logTextBox.ReadOnly = true;
+			this.logTextBox.Size = new System.Drawing.Size(528, 424);
+			this.logTextBox.TabIndex = 3;
+			this.logTextBox.Text = "";
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.tableLayoutPanel5);
+			this.panel2.Controls.Add(this.tableLayoutPanel4);
+			this.panel2.Controls.Add(this.tableLayoutPanel3);
+			this.panel2.Controls.Add(this.tableLayoutPanel2);
+			this.panel2.Controls.Add(this.tableLayoutPanel1);
+			this.panel2.Controls.Add(this.label3);
+			this.panel2.Controls.Add(this.label2);
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Controls.Add(this.textBox2);
+			this.panel2.Controls.Add(this.textBox1);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+			this.panel2.Location = new System.Drawing.Point(528, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(272, 424);
+			this.panel2.TabIndex = 2;
+			// 
+			// tableLayoutPanel5
+			// 
+			this.tableLayoutPanel5.ColumnCount = 2;
+			this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel5.Controls.Add(this.label7, 0, 0);
+			this.tableLayoutPanel5.Controls.Add(this.lastFMUsername, 1, 0);
+			this.tableLayoutPanel5.Dock = System.Windows.Forms.DockStyle.Top;
+			this.tableLayoutPanel5.Location = new System.Drawing.Point(0, 105);
+			this.tableLayoutPanel5.Name = "tableLayoutPanel5";
+			this.tableLayoutPanel5.RowCount = 1;
+			this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.8F));
+			this.tableLayoutPanel5.Size = new System.Drawing.Size(272, 35);
+			this.tableLayoutPanel5.TabIndex = 18;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label7.Location = new System.Drawing.Point(3, 0);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(130, 35);
+			this.label7.TabIndex = 0;
+			this.label7.Text = "LastFM Username";
+			this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// lastFMUsername
+			// 
+			this.lastFMUsername.Location = new System.Drawing.Point(139, 3);
+			this.lastFMUsername.Name = "lastFMUsername";
+			this.lastFMUsername.Size = new System.Drawing.Size(125, 27);
+			this.lastFMUsername.TabIndex = 1;
+			// 
+			// tableLayoutPanel4
+			// 
+			this.tableLayoutPanel4.ColumnCount = 2;
+			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel4.Controls.Add(this.label6, 0, 0);
+			this.tableLayoutPanel4.Controls.Add(this.discordAppId, 1, 0);
+			this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Top;
+			this.tableLayoutPanel4.Location = new System.Drawing.Point(0, 70);
+			this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+			this.tableLayoutPanel4.RowCount = 1;
+			this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.8F));
+			this.tableLayoutPanel4.Size = new System.Drawing.Size(272, 35);
+			this.tableLayoutPanel4.TabIndex = 17;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label6.Location = new System.Drawing.Point(3, 0);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(130, 35);
+			this.label6.TabIndex = 0;
+			this.label6.Text = "Discord App Id";
+			this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// discordAppId
+			// 
+			this.discordAppId.Location = new System.Drawing.Point(139, 3);
+			this.discordAppId.Name = "discordAppId";
+			this.discordAppId.Size = new System.Drawing.Size(125, 27);
+			this.discordAppId.TabIndex = 1;
+			this.discordAppId.Text = "775484756380352542";
+			// 
+			// tableLayoutPanel3
+			// 
+			this.tableLayoutPanel3.ColumnCount = 2;
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel3.Controls.Add(this.label5, 0, 0);
+			this.tableLayoutPanel3.Controls.Add(this.lastFMSecret, 1, 0);
+			this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Top;
+			this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 35);
+			this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+			this.tableLayoutPanel3.RowCount = 1;
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.8F));
+			this.tableLayoutPanel3.Size = new System.Drawing.Size(272, 35);
+			this.tableLayoutPanel3.TabIndex = 16;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label5.Location = new System.Drawing.Point(3, 0);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(130, 35);
+			this.label5.TabIndex = 0;
+			this.label5.Text = "LastFM Secret";
+			this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// lastFMSecret
+			// 
+			this.lastFMSecret.Location = new System.Drawing.Point(139, 3);
+			this.lastFMSecret.Name = "lastFMSecret";
+			this.lastFMSecret.Size = new System.Drawing.Size(125, 27);
+			this.lastFMSecret.TabIndex = 1;
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.ColumnCount = 2;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel2.Controls.Add(this.label4, 0, 0);
+			this.tableLayoutPanel2.Controls.Add(this.lastFMKey, 1, 0);
+			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 1;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.8F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(272, 35);
+			this.tableLayoutPanel2.TabIndex = 15;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label4.Location = new System.Drawing.Point(3, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(130, 35);
+			this.label4.TabIndex = 0;
+			this.label4.Text = "LastFM Key";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// lastFMKey
+			// 
+			this.lastFMKey.Location = new System.Drawing.Point(139, 3);
+			this.lastFMKey.Name = "lastFMKey";
+			this.lastFMKey.Size = new System.Drawing.Size(125, 27);
+			this.lastFMKey.TabIndex = 1;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Controls.Add(this.start, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.stop, 0, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 389);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.8F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(272, 35);
+			this.tableLayoutPanel1.TabIndex = 8;
+			// 
+			// start
+			// 
+			this.start.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.start.Location = new System.Drawing.Point(139, 3);
+			this.start.Name = "start";
+			this.start.Size = new System.Drawing.Size(130, 29);
+			this.start.TabIndex = 2;
+			this.start.Text = "Start";
+			this.start.UseVisualStyleBackColor = true;
+			this.start.Click += new System.EventHandler(this.start_Click);
+			// 
+			// stop
+			// 
+			this.stop.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.stop.Location = new System.Drawing.Point(3, 3);
+			this.stop.Name = "stop";
+			this.stop.Size = new System.Drawing.Size(130, 29);
+			this.stop.TabIndex = 1;
+			this.stop.Text = "Stop";
+			this.stop.UseVisualStyleBackColor = true;
+			this.stop.Click += new System.EventHandler(this.stop_Click);
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(0, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 10;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(0, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 11;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 12;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(0, 0);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 27);
+			this.textBox2.TabIndex = 13;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(0, 0);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 27);
+			this.textBox1.TabIndex = 14;
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.statusStrip1);
+			this.Name = "Main";
+			this.Text = "Main";
+			this.Resize += new System.EventHandler(this.Main_Resize);
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.tableLayoutPanel5.ResumeLayout(false);
+			this.tableLayoutPanel5.PerformLayout();
+			this.tableLayoutPanel4.ResumeLayout(false);
+			this.tableLayoutPanel4.PerformLayout();
+			this.tableLayoutPanel3.ResumeLayout(false);
+			this.tableLayoutPanel3.PerformLayout();
+			this.tableLayoutPanel2.ResumeLayout(false);
+			this.tableLayoutPanel2.PerformLayout();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.NotifyIcon notifier;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.ContextMenuStrip notifyMenuStrip;
+		private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+		private System.Windows.Forms.RichTextBox logTextBox;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Button start;
+		private System.Windows.Forms.Button stop;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox discordAppId;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox lastFMSecret;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox lastFMKey;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel5;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox lastFMUsername;
+	}
+}
---
+++ src/Main.cs
@@ -0,0 +1,236 @@
+﻿using DiscordRPC;
+using IF.Lastfm.Core.Api;
+using IF.Lastfm.Core.Objects;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LastRPC
+{
+	public partial class Main : Form
+	{
+		string configLocation = Path.Combine(AppContext.BaseDirectory, "config.json");
+		Config config;
+		LastfmClient lastfmClient;
+		DiscordRpcClient rpcClient;
+		Thread lastFMThread;
+		LastTrack currentTrack;
+		bool isRunning = false;
+
+		public Main()
+		{
+			if (File.Exists(configLocation))
+			{
+				config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(configLocation));
+			}
+
+			InitializeComponent();
+
+			if (config != null)
+			{
+				lastFMKey.Text = config.LastFMKey;
+				lastFMSecret.Text = config.LastFMSecret;
+				discordAppId.Text = config.DiscordId;
+				lastFMUsername.Text = config.Username;
+			}
+			else
+			{
+				config = new Config();
+			}
+
+			stop.Enabled = false;
+
+			statusLabel.Text = "Ready";
+		}
+
+		private void notifier_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			Show();
+			WindowState = FormWindowState.Normal;
+		}
+
+		private void Main_Resize(object sender, System.EventArgs e)
+		{
+			if (WindowState == FormWindowState.Minimized)
+			{
+				Hide();
+			}
+		}
+
+		private void Configure()
+		{
+			lastfmClient = new LastfmClient(lastFMKey.Text, lastFMSecret.Text);
+
+			rpcClient = new DiscordRpcClient(discordAppId.Text ?? "775484756380352542");
+			config.Set(lastFMKey.Text, lastFMSecret.Text, discordAppId.Text, lastFMUsername.Text);
+
+			File.WriteAllText(configLocation, JsonConvert.SerializeObject(config));
+		}
+
+		private string GetLogMessage(string status, string key, string message)
+			=> $"[{DateTime.Now:yyyy/MM/dd HH:mm:ss}] [{status}] [{key}] {message}";
+
+		private void WriteLog(string message)
+		{
+			if (InvokeRequired)
+			{
+				this.Invoke(new Action<string>(WriteLog), new object[] { message });
+				return;
+			}
+
+			logTextBox.AppendText(message);
+			logTextBox.AppendText(Environment.NewLine);
+		}
+
+		private void StartLastFMScrobbler()
+		{
+			string username = lastFMUsername.Text;
+			statusLabel.Text = "Running";
+			isRunning = true;
+			lastFMThread = new Thread(async () =>
+			{
+				Thread.CurrentThread.IsBackground = true;
+
+				while (isRunning)
+				{
+					WriteLog(GetLogMessage("I", "LastFM Request", "Getting User Scrobbles"));
+
+					var scrobbles = await lastfmClient.User.GetRecentScrobbles(username, extendedResponse: true, count: 1);
+
+					if (scrobbles.Success)
+					{
+						if (scrobbles.Content.Count > 0)
+						{
+							var mostRecent = scrobbles.Content[0];
+
+							if (mostRecent.IsNowPlaying.HasValue && mostRecent.IsNowPlaying.Value)
+							{
+								WriteLog(GetLogMessage("I", "LastFM Request", "Found Currently playing"));
+
+								if (currentTrack == null || mostRecent.Mbid != currentTrack.Mbid)
+								{
+									WriteLog(GetLogMessage("I", "LastFM Request", "Different song, setting Status"));
+									SetDiscordStatus(mostRecent, true);
+								}
+							}
+
+							currentTrack = mostRecent;
+						}
+						else
+						{
+							WriteLog(GetLogMessage("E", "LastFM Request", "Got Nothing Waiting 30s"));
+						}
+					}
+					else
+					{
+						WriteLog(GetLogMessage("E", "LastFM Request", "Wasn't successful, waiting 30s"));
+					}
+
+					await Task.Delay(TimeSpan.FromSeconds(30)).ConfigureAwait(false);
+				}
+
+				WriteLog(GetLogMessage("I", "LastFM Request", "Got outside, stopping"));
+			});
+
+			lastFMThread.Start();
+		}
+
+		private void StopLastFMScrobbler()
+		{
+			isRunning = false;
+			statusLabel.Text = "Ready";
+		}
+
+		private void SetDiscordStatus(LastTrack track, bool isPlaying)
+		{
+			bool didInit = false;
+			if (!rpcClient.IsInitialized)
+			{
+				WriteLog(GetLogMessage("I", "Discord RPC", "Not Initialised"));
+				if (rpcClient.Initialize())
+				{
+					WriteLog(GetLogMessage("I", "Discord RPC", "Successfully Initialised"));
+					didInit = true;
+				}
+				else
+				{
+					WriteLog(GetLogMessage("E", "Discord RPC", "Failure initialising, is Discord running?"));
+				}
+			}
+			else
+			{
+				didInit = true;
+			}
+
+			if (didInit)
+			{
+				Timestamps stamps = null;
+
+				if (track.TimePlayed.HasValue && track.Duration.HasValue)
+				{
+					var end = track.TimePlayed.Value.Add(track.Duration.Value);
+
+					stamps = new Timestamps
+					{
+						EndUnixMilliseconds = Convert.ToUInt64(end.ToUnixTimeMilliseconds())
+					};
+				}
+
+				rpcClient.SetPresence(new RichPresence
+				{
+					Assets = new Assets
+					{
+						LargeImageKey = "lastfm",
+						SmallImageKey = isPlaying ? "playing" : "paused"
+					},
+					Details = $"{track.ArtistName} - {track.Name}",
+					Timestamps = stamps
+				});
+
+				WriteLog(GetLogMessage("I", "Discord RPC", "Set Status"));
+			}
+		}
+
+		private void start_Click(object sender, System.EventArgs e)
+		{
+			discordAppId.ReadOnly = true;
+			lastFMKey.ReadOnly = true;
+			lastFMSecret.ReadOnly = true;
+			lastFMUsername.ReadOnly = true;
+			Configure();
+			StartLastFMScrobbler();
+			start.Enabled = false;
+			stop.Enabled = true;
+		}
+
+		private void stop_Click(object sender, System.EventArgs e)
+		{
+			discordAppId.ReadOnly = false;
+			lastFMKey.ReadOnly = false;
+			lastFMSecret.ReadOnly = false;
+			lastFMUsername.ReadOnly = false;
+			StopLastFMScrobbler();
+			start.Enabled = true;
+			stop.Enabled = false;
+		}
+
+		public class Config
+		{
+			public string LastFMKey;
+			public string LastFMSecret;
+			public string DiscordId;
+			public string Username;
+
+			public void Set(string key, string secret, string id, string username)
+			{
+				LastFMKey = key;
+				LastFMSecret = secret;
+				DiscordId = id;
+				Username = username;
+			}
+		}
+	}
+}
